b4d4fd9de25c925386ec54449472e57e8b9bafe5
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-08-17 21:35:05 -0300

    Refactoring the long_exposure code

--- long_exposure.py
+++ long_exposure.py
@@ -1,90 +1,107 @@
 #!/usr/bin/env python
 
 import argparse
-import imutils
 import cv2
 import sys
 
-def print_progress(iteration, total, prefix='Progress:', suffix='Complete', decimals=2, bar_length=50):
-    str_format = "{0:." + str(decimals) + "f}"
-    percents = str_format.format(100 * (iteration / float(total)))
-    filled_length = int(round(bar_length * iteration / float(total)))
-    bar = '#' * filled_length + '-' * (bar_length - filled_length)
 
-    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix))
-    
-    if iteration >= total:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
+class ProgressBar(object):
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", required=True,
-    help="Path to input video file")
-ap.add_argument("-o", "--output", required=True,
-    help="Path to output 'long exposure' image")
-ap.add_argument("-s", "--step", type=int, default=1,
-    help="Step used to get the frames")
-args = vars(ap.parse_args())
+    def __init__(self, total, prefix='Progress:', suffix='Complete', decimals=2, bar_length=50):
+        self.total = total
+        self.prefix = prefix
+        self.suffix = suffix
+        self.decimals = decimals
+        self.bar_length = bar_length
 
-# initialize the Red, Green, and Blue channel averages, along with
-# the total number of frames read from the file
-(rAvg, gAvg, bAvg) = (None, None, None)
-total = 0
+    def update(self, progress):
+        str_format = "{0:." + str(self.decimals) + "f}"
+        percents = str_format.format(100 * (progress / float(self.total)))
+        filled_length = int(round(self.bar_length * progress / float(self.total)))
+        bar = '#' * filled_length + '-' * (self.bar_length - filled_length)
 
-# open a pointer to the video file
-print("[INFO] Opening video file pointer...")
-stream = cv2.VideoCapture(args["video"])
-print("[INFO] Computing frame averages (this will take awhile)...")
+        sys.stdout.write('\r%s |%s| %s%s %s' % (self.prefix, bar, percents, '%', self.suffix))
 
-# Get the total number of frames to show the progress bar
-total_frames = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
+        if progress >= self.total:
+            sys.stdout.write('\n')
+        sys.stdout.flush()
 
-# Start the progress bar as zero
-count = 0
-print_progress(total, total_frames)
 
-# Get the step that will be used to get the frames
-step = args["step"]
+class LongExposure(object):
 
-# loop over frames from the video file stream
-while True:
-    # grab the frame from the file stream
-    (grabbed, frame) = stream.read()
+    @staticmethod
+    def run(video, output):
 
-    # if the frame was not grabbed, then we have reached the end of
-    # the sfile
-    if not grabbed:
-        break
+        # initialize the Red, Green, and Blue channel averages, along with
+        # the total number of frames read from the file
+        (r_avg, g_avg, b_avg) = (None, None, None)
+        total = 0
 
-    if count % step == 0:
-        # otherwise, split the frmae into its respective channels
-        (B, G, R) = cv2.split(frame.astype("float"))
+        # open a pointer to the video file
+        print("[INFO] Opening video file pointer...")
+        stream = cv2.VideoCapture(video)
+        print("[INFO] Computing frame averages (this will take awhile)...")
 
-        # if the frame averages are None, initialize them
-        if rAvg is None:
-            rAvg = R
-            bAvg = B
-            gAvg = G
-        # otherwise, compute the weighted average between the history of
-        # frames and the current frames
+        # Get the total number of frames to show the progress bar
+        total_frames = int(stream.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        progress_bar = ProgressBar(total_frames)
+        # Start the progress bar as zero
+        count = 0
+        progress_bar.update(0)
+
+        # Get the step that will be used to get the frames
+        step = args["step"]
+
+        # loop over frames from the video file stream
+        while True:
+            # grab the frame from the file stream
+            (grabbed, frame) = stream.read()
+
+            # if the frame was not grabbed, then we have reached the end of
+            # the file
+            if not grabbed:
+                break
+
+            if count % step == 0:
+                # otherwise, split the frame into its respective channels
+                (B, G, R) = cv2.split(frame.astype("float"))
+
+                # if the frame averages are None, initialize them
+                if r_avg is None:
+                    r_avg = R
+                    b_avg = B
+                    g_avg = G
+                # otherwise, compute the weighted average between the history of
+                # frames and the current frames
+                else:
+                    r_avg = ((total * r_avg) + (1 * R)) / (total + 1.0)
+                    g_avg = ((total * g_avg) + (1 * G)) / (total + 1.0)
+                    b_avg = ((total * b_avg) + (1 * B)) / (total + 1.0)
+
+                # increment the total number of frames read thus far
+                total += 1
+            count += 1
+
+            progress_bar.update(count)
+
+        progress_bar.update(total_frames)
+        if total > 0:
+            # merge the RGB averages together and write the output image to disk
+            avg = cv2.merge([b_avg, g_avg, r_avg]).astype("uint8")
+            cv2.imwrite(output, avg)
         else:
-            rAvg = ((total * rAvg) + (1 * R)) / (total + 1.0)
-            gAvg = ((total * gAvg) + (1 * G)) / (total + 1.0)
-            bAvg = ((total * bAvg) + (1 * B)) / (total + 1.0)
+            print("[ERRO] No frames found...")
 
-        # increment the total number of frames read thus far
-        total += 1
-    count += 1
-    print_progress(count, total_frames)
+        # do a bit of cleanup on the file pointer
+        stream.release()
 
-print_progress(total_frames, total_frames)
-if total > 0:
-    # merge the RGB averages together and write the output image to disk
-    avg = cv2.merge([bAvg, gAvg, rAvg]).astype("uint8")
-    cv2.imwrite(args["output"], avg)
-else:
-    print("[ERRO] No frames found...")
-
-# do a bit of cleanup on the file pointer
-stream.release()
+if __name__ == "__main__":
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", required=True, help="Path to input video file")
+    ap.add_argument("-o", "--output", required=True, help="Path to output 'long exposure' image")
+    ap.add_argument("-s", "--step", type=int, default=1, help="Step used to get the frames")
+    args = vars(ap.parse_args())
+    long_exposure = LongExposure()
+    long_exposure.run(args["video"], args["output"])
